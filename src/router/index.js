b933e5f9e8c7370e9a19633a27baa2c75a84b6a1
Bert Cafecito <189543060+bertcafecito@users.noreply.github.com>
2026-06-10 12:29:52 +0000

    Add Events Page

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/events',
+      name: 'events',
+      component: () => import('../views/EventsView.vue'),
+    }
   ],
 })
 
